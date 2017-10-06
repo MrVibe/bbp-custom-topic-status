23bd980f580184c78e27fc9a2d676ed24c8a3a43
Ripul-Mac <kumar.ripul@gmail.com>
2017-10-06 13:05:16 +0530

    init
    starting out the plugin and creating files.

---
+++ bbp_custom_topic_status.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Custom Topic Status
+Plugin URI: http://www.Vibethemes.com
+Description: Add custom topic status in BBPRess
+Version: 1.0
+Author: VibeThemes
+Author URI: http://www.vibethemes.com
+License: GPLv2
+Text Domain: bbpcts
+Domain Path: /languages/
+*/
